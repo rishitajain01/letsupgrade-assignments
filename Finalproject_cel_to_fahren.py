018ad84cca7f2b334325cabfc15ab8c8ae16fb5d
rishitajain01 <71720750+rishitajain01@users.noreply.github.com>
2021-08-01 22:57:19 +0530

    Final project submitted

---
+++ Finalproject_cel_to_fahren.py
@@ -0,0 +1,5 @@
+#PROJECT FINAL
+#degree converter degree celcius to fahrenheit
+c=int(input("Enter the value of temperature in degree Celcius "))
+f=(9*c/5)+32
+print("Temperature in degree fahrenheit=",f)
